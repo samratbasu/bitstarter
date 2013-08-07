2e9ab03da3591fb2c16df9f0c0747f24cef1ca78
Samrat Basu <SamratBasu@yahoo.com>
2013-08-07 05:23:25 +0000

    edited prouct desc

--- grader.js
+++ grader.js
@@ -20,12 +20,14 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
+var sys = require('util');
+var rest = require('restler'); 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://shrouded-journey-6052.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,14 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+var assertLinkExists = rest.get(URL_DEFAULT).on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    var cheerioHtmlFile = result;
+  }
+});
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,8 +75,9 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-u, --url <url_link>', 'Link to url', URL_DEFAULT)
+	.parse(process.argv);
+    var checkJson = checkHtmlFile(program.file, program.checks, program.linkUrl);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var readBuff = fs.readFileSync('index.html');
 var writeTxt = readBuff.toString();
-console.log(writeTxt);
+//console.log(writeTxt);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
